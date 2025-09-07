1eac64e889a739f87bf1a9f714a9248b87f76bb1
Nikhil Y <nikhil.y@incadea.com>
2025-09-07 21:15:24 +0530

    newchanges done to branch

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -16,7 +16,7 @@ function AboutUs() {
       name: "CEO",
       image: ceo,
       description:
-      //changes
+      //changes done in new branch
         "The CEO of Tesla oooo is responsible for shaping the company's vision of accelerating the world's transition to sustainable energy. They oversee all aspects of Tesla's operations, from electric vehicle production to energy solutions, ensuring profitability and long-term growth. The CEO actively engages with investors, government bodies, and the public, driving innovation and strategic initiatives. They represent Tesla in key decisions, partnerships, and global expansion efforts while maintaining the company’s mission-driven culture.",
     },
     {
